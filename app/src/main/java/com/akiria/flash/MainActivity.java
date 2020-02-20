84630eea77f8036759dd8b631c7b645058185291
akrria <akiria.anderson@gmail.com>
2020-02-20 10:38:33 -0500

    second commit

--- app/src/main/java/com/akiria/flash/MainActivity.java
+++ app/src/main/java/com/akiria/flash/MainActivity.java
@@ -21,25 +21,27 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.correct_green, null));
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.correct_backg);
             }
         });
         findViewById(R.id.wrong_answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.wrong_answer1).setBackgroundColor(getResources().getColor(R.color.wrong_red, null));
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.correct_green, null));
+                findViewById(R.id.wrong_answer1).setBackgroundResource(R.drawable.wrong_backg);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.correct_backg);
 
             }
         });
         findViewById(R.id.wrong_answer2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.wrong_answer2).setBackgroundColor(getResources().getColor(R.color.wrong_red, null));
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.correct_green, null));
+                findViewById(R.id.wrong_answer2).setBackgroundResource(R.drawable.wrong_backg);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.correct_backg);
 
             }
         });
+
+
 
     }
 
